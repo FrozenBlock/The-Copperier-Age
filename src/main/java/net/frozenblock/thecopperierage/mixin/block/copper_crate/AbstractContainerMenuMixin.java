6aa265951397d12d10cde5020c4f5411e7d89824
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-12-19 15:48:53 -0500

    dont use client mixin for bundle fail sounds

--- src/main/java/net/frozenblock/thecopperierage/mixin/block/copper_crate/AbstractContainerMenuMixin.java
+++ src/main/java/net/frozenblock/thecopperierage/mixin/block/copper_crate/AbstractContainerMenuMixin.java
@@ -1,30 +1,17 @@
-/*
- * Copyright 2025 FrozenBlock
- * This file is part of The Copperier Age.
- *
- * This program is free software; you can modify it under
- * the terms of version 1 of the FrozenBlock Modding Oasis License
- * as published by FrozenBlock Modding Oasis.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * FrozenBlock Modding Oasis License for more details.
- *
- * You should have received a copy of the FrozenBlock Modding Oasis License
- * along with this program; if not, see <https://github.com/FrozenBlock/Licenses>.
- */
-
 package net.frozenblock.thecopperierage.mixin.block.copper_crate;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.frozenblock.thecopperierage.block.entity.inventory.CrateSlot;
+import net.frozenblock.thecopperierage.registry.TCASounds;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.SlotAccess;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ClickAction;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.BundleItem;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -42,7 +29,13 @@ public class AbstractContainerMenuMixin {
 	private boolean theCopperierAge$preventBundlingInCrateSlots(
 		ItemStack instance, ItemStack otherStack, Slot slot, ClickAction clickAction, Player player, SlotAccess slotAccess, Operation<Boolean> original
 	) {
-		if (slot instanceof CrateSlot) return false;
+		if (slot instanceof CrateSlot) {
+			if (instance.has(DataComponents.BUNDLE_CONTENTS) && !otherStack.isEmpty() && !otherStack.has(DataComponents.BUNDLE_CONTENTS)) {
+				player.level().playPlayerSound(TCASounds.UI_CRATE_CLICK_FAIL.value(), SoundSource.UI, 0.25F, 1F);
+				BundleItem.playInsertFailSound(player);
+			}
+			return false;
+		}
 		return original.call(instance, otherStack, slot, clickAction, player, slotAccess);
 	}
 

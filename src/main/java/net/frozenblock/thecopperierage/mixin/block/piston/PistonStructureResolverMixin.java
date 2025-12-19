ba7b5890084ed4ac1f63e209b1562471ee4735e9
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-12-19 16:23:50 -0500

    lol

--- src/main/java/net/frozenblock/thecopperierage/mixin/block/piston/PistonStructureResolverMixin.java
+++ src/main/java/net/frozenblock/thecopperierage/mixin/block/piston/PistonStructureResolverMixin.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2025 FrozenBlock
+ * This file is part of The Copperier Age.
+ *
+ * This program is free software; you can modify it under
+ * the terms of version 1 of the FrozenBlock Modding Oasis License
+ * as published by FrozenBlock Modding Oasis.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * FrozenBlock Modding Oasis License for more details.
+ *
+ * You should have received a copy of the FrozenBlock Modding Oasis License
+ * along with this program; if not, see <https://github.com/FrozenBlock/Licenses>.
+ */
+
 package net.frozenblock.thecopperierage.mixin.block.piston;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -25,7 +42,7 @@ public class PistonStructureResolverMixin {
 			target = "Lnet/minecraft/world/level/block/piston/PistonStructureResolver;isSticky(Lnet/minecraft/world/level/block/state/BlockState;)Z"
 		)
 	)
-	private boolean theCopperierAge$doubleChestStickingA(BlockState state, Operation<Boolean> original) {
+	private boolean theCopperierAge$doubleChestSticking(BlockState state, Operation<Boolean> original) {
 		if (state.is(ConventionalBlockTags.CHESTS) && state.is(TCABlockTags.HAS_PUSHABLE_BLOCK_ENTITY)) return true;
 		return original.call(state);
 	}

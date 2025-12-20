eecd1edfed909c3275640845e84c90e7f62deea7
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-12-19 20:46:21 -0500

    use flib pushable BEs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,7 @@ pluginManagement {
 
 rootProject.name = "The Copperier Age"
 
-localRepository("FrozenLib", "maven.modrinth:frozenlib", true, false)
+localRepository("FrozenLib", "maven.modrinth:frozenlib", true, true)
 
 
 fun localRepository(repo: String, dependencySub: String, kotlin: Boolean, enabled: Boolean) {
--- src/main/java/net/frozenblock/thecopperierage/datagen/tag/TCABlockTagProvider.java
+++ src/main/java/net/frozenblock/thecopperierage/datagen/tag/TCABlockTagProvider.java
@@ -105,7 +105,7 @@ public final class TCABlockTagProvider extends FabricTagProvider.BlockTagProvide
 		this.builder(TCABlockTags.WRENCH_CANNOT_ROTATE)
 			.addOptional(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("trailiertales", "coffin")));
 
-		this.valueLookupBuilder(TCABlockTags.HAS_PUSHABLE_BLOCK_ENTITY)
+		this.valueLookupBuilder(FrozenBlockTags.HAS_PUSHABLE_BLOCK_ENTITY)
 			.addOptionalTag(TCABlockTags.COPPER_CRATES);
 
 		this.builder(FrozenBlockTags.BLOWING_CAN_PASS_THROUGH)

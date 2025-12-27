19b0e09e02cb151bc57b77355da019006a0ff880
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2025-12-27 14:02:40 -0500

    we dont need this lol

--- src/main/java/net/frozenblock/thecopperierage/mixin/client/oxidizing_auto_models/ModelBakeryMixin.java
+++
@@ -1,8 +0,0 @@
-package net.frozenblock.thecopperierage.mixin.client.oxidizing_auto_models;
-
-import net.minecraft.client.resources.model.ModelBakery;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ModelBakery.class)
-public class ModelBakeryMixin {
-}

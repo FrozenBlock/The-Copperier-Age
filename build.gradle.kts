fe664595eb00c47ae5d8b38310078b7b1669dc4d
Ethan Stokes <erstokes10@gmail.com>
2026-06-15 19:11:15 -0500

    26.2-rc-2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
 }
 
 plugins {
-    id("net.fabricmc.fabric-loom") version("1.16-SNAPSHOT")
+    id("net.fabricmc.fabric-loom") version("1.17-SNAPSHOT")
     id("org.quiltmc.gradle.licenser") version("+")
     id("org.ajoberstar.grgit") version("+")
     id("com.modrinth.minotaur") version("+")
@@ -168,10 +168,10 @@ dependencies {
     api("maven.modrinth:frozenlib:$frozenlib_version")
 
     // Mod Menu
-    implementation("maven.modrinth:modmenu:$modmenu_version")
+    compileOnly("maven.modrinth:modmenu:$modmenu_version")
 
     // Cloth Config
-    implementation("me.shedaniel.cloth:cloth-config-fabric:$cloth_config_version") {
+    compileOnly("me.shedaniel.cloth:cloth-config-fabric:$cloth_config_version") {
         exclude(group = "net.fabricmc.fabric-api")
         exclude(group = "com.terraformersmc")
     }
